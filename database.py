8ea04ad541b09f3530a22c1a9de4c052e9ee1a26
hetul21 <hetul.patel21004@gmail.com>
2026-01-15 17:52:47 -0500

    Phase 1: Database schema and table creation

---
+++ database.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+def create_database():
+    conn = sqlite3.connect("sales.db")
+    cursor = conn.cursor()
+
+    cursor.executescript("""create table if not exists customers(
+                         id INTEGER PRIMARY KEY AUTOINCREMENT,
+                         name text not null,
+                         email text,
+                        city text);
+                         create table if not exists products(
+                         id INTEGER PRIMARY KEY AUTOINCREMENT,
+                         name text not null,
+                         category text,
+                         price real not null);
+
+                         create table if not exists orders(
+                         id INTEGER PRIMARY KEY AUTOINCREMENT,
+                         customer_id integer,
+                         order_date text,
+                         FOREIGN KEY (customer_id) REFERENCES customers(id));
+                         create table if not exists order_items(
+                         id INTEGER PRIMARY KEY AUTOINCREMENT,
+                         order_id integer,
+                         product_id integer,
+                         quantity intteger,
+                         FOREIGN KEY (order_id) REFERENCES orderS(id),
+                         FOREIGN KEY (product_id) REFERENCES products(id)
+                         );
+                         
+
+                         """)
+    conn.commit()
+    conn.close()
+
+
+if __name__ == "__main__":
+    create_database()
+    print("Database created successfully!")
